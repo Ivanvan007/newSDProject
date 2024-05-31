87c7938c24b30d580d9503754005aea667edb056
Ivanvan007 <ivan.ibanhez.vilar@gmail.com>
2024-05-31 07:28:28 +0100

    LastChanges-31-05-6:28

--- ursoDB/src/controllers/serverTemplate.js
+++ ursoDB/src/controllers/serverTemplate.js
@@ -1,21 +1,19 @@
 const express = require('express');
-const createLogger = require('../node_modules/logger/logger');
+const {createLogger} = require('../node_modules/logger/logger');
+
+let app = express();
+let logger;
 
 function createServer(port, routes) {
-  let app = express();
-  let logger = createLogger(port);
-
-  app.use(express.json());
-
-  routes.forEach(route => {
-    app[route.method](route.path, route.handler);
-  });
-
-  app.listen(port, () => {
-    logger.info(`Server started on port ${port}`);
-  });
-
-  return app;
+    logger = createLogger(port)
+    
+    app.use(express.json());
+    routes.forEach(route => {app[route.method](route.path, route.handler);
+    });
+    app.listen(port, () => {
+        logger.info(`Server started on port ${port}`);
+    });
+    return app;
 }
 
 module.exports = { createServer };
--- ursoDB/src/manager.js
+++ ursoDB/src/manager.js
@@ -3,15 +3,33 @@
 const { exec } = require('child_process');
 const path = require('path');
 const config = require('./../etc/configure.json');
-const foreverPath = path.join(__dirname, './node_modules/utils/node_modules/.bin/forever');
+
+const foreverPath = path.resolve(__dirname, './node_modules/utils/node_modules/.bin/forever');
+const rpScriptPath = path.resolve(__dirname, './RP/RP.js');
+const rpScriptPathNoContext = path.join("","/RP/RP.js")
+const serverScriptPath = path.resolve(__dirname, './DNs/server.js');
+const serverScriptPathNoContext = path.join("","/DNs/server.js")
 
 const servers = config.DNs.flatMap(dn => dn.servers.map(server => `http://${server.host}:${server.port}`));
 
-function startServers() {
+async function startRP() {
+  const command = `${foreverPath} start --minUptime 1000 --spinSleepTime 1000 ${rpScriptPathNoContext}`;
+  console.log(`Executing: ${command}`);
+  exec(command, { env: { PATH: `${process.env.PATH};${path.join(__dirname, '../node_modules/utils/node_modules/.bin')}` } }, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error starting reverse proxy: ${stderr}`);
+    } else {
+      console.log(`Reverse proxy started: ${stdout}`);
+      startServers();  // Start data node servers after reverse proxy
+    }
+  });
+}
+
+async function startServers() {
   servers.forEach(server => {
-    exec(`${foreverPath} start ./DNs/server.js ${server}`,
-    { env: { PATH: `${process.env.PATH}:${path.join(__dirname, './node_modules/utils/node_modules/.bin')}` } },
-    (err, stdout, stderr) => {
+    const command = `${foreverPath} start --minUptime 1000 --spinSleepTime 1000 ${serverScriptPathNoContext} ${server}`;
+    console.log(`Executing: ${command}`);
+    exec(command, { env: { PATH: `${process.env.PATH};${path.join(__dirname, '../node_modules/utils/node_modules/.bin')}` } }, (err, stdout, stderr) => {
       if (err) {
         console.error(`Error starting server ${server}: ${stderr}`);
       } else {
@@ -21,11 +39,24 @@ function startServers() {
   });
 }
 
-function stopServers() {
+async function stopRP() {
+  const command = `${foreverPath} stop ${rpScriptPathNoContext}`;
+  console.log(`Executing: ${command}`);
+  exec(command, { env: { PATH: `${process.env.PATH};${path.join(__dirname, '../node_modules/utils/node_modules/.bin')}` } }, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error stopping reverse proxy: ${stderr}`);
+    } else {
+      console.log(`Reverse proxy stopped: ${stdout}`);
+      stopServers();  // Stop data node servers after reverse proxy
+    }
+  });
+}
+
+async function stopServers() {
   servers.forEach(server => {
-    exec(`${foreverPath} stop ./DNs/server.js ${server}`,
-    { env: { PATH: `${process.env.PATH}:${path.join(__dirname, './node_modules/utils/node_modules/.bin')}` } },
-    (err, stdout, stderr) => {
+    const command = `${foreverPath} stop ${serverScriptPathNoContext} ${server}`;
+    console.log(`Executing: ${command}`);
+    exec(command, { env: { PATH: `${process.env.PATH};${path.join(__dirname, '../node_modules/utils/node_modules/.bin')}` } }, (err, stdout, stderr) => {
       if (err) {
         console.error(`Error stopping server ${server}: ${stderr}`);
       } else {
@@ -35,17 +66,17 @@ function stopServers() {
   });
 }
 
-function restartServers() {
-  stopServers();
-  startServers();
+async function restartServers() {
+  await stopRP();
+  startRP();
 }
 
 const action = process.argv[2];
 
 if (action === 'start') {
-  startServers();
+  startRP();
 } else if (action === 'stop') {
-  stopServers();
+  stopRP();
 } else if (action === 'restart') {
   restartServers();
 } else {
