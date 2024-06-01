7aebb84687726cfe17cf6d7780a73a8e757037f6
Ivanvan007 <ivan.ibanhez.vilar@gmail.com>
2024-06-01 10:29:02 +0100

    Last Changes 01-06 10:28

--- ursoDB/src/DNs/raft.js
+++ ursoDB/src/DNs/raft.js
@@ -1,3 +1,4 @@
+"use strict"
 const axios = require('axios');
 const loggerTemp = require('../node_modules/logger/logger');
 const config = require('../../etc/configure.json');
@@ -22,28 +23,36 @@ class Raft {
   }
 
   async startElection() {
-    this.state = 'candidate';
-    this.votesReceived++;
-    const dn = this.DNs.find(dn => dn.servers.some(server => server.port === this.port));
-    dn.servers.forEach(server => {
-      if (server.port !== this.port) {
-        axios.get(`http://${server.host}:${server.port}/election`).then(response => {
-          if (response.data.voteGranted) {
-            this.logger.info(`Server on ${this.port} has voted`)
-            //this.votesReceived++;
-          }
-        }).catch(error => {
-          logger.error(`Server on port ${this.port} election error: ${error}`);
-        });
+    try
+    {
+      this.state = 'candidate';
+      this.votesReceived++;
+      const dn = this.DNs.find(dn => dn.servers.some(server => server.port === this.port));
+      dn.servers.forEach(server => {
+        if (server.port !== this.port) {
+          axios.get(`http://${server.host}:${server.port}/election`).then(response => {
+
+            if (response.data.voteGranted) {
+              this.logger.info(`Server on ${this.port} has voted`)
+              //this.votesReceived++;
+            }
+          }).catch(error => {
+            logger.error(`Server on port ${this.port} election error: ${error}`);
+          });
+        }
+      });
+      if (this.votesReceived > dn.servers.length / 2) {
+        this.state = 'leader';
+        this.leader = `http://${server.host}:${server.port}`;
+        this.state = 'leader';
+        this.logger.info(`Server on port ${this.port} elected as leader`);
+        this.notifyRP();
       }
-    });
-    if (this.votesReceived > dn.servers.length / 2) {
-      this.state = 'leader';
-      this.leader = `http://${server.host}:${server.port}`;
-      this.state = 'leader';
-      this.logger.info(`Server on port ${this.port} elected as leader`);
-      this.notifyRP();
+    } catch(error)
+    {
+      this.logger.error(`Server on port ${this.port} couldnot be elected as leader cause of ${error}`);
     }
+    
   }
 
   async notifyRP() {
